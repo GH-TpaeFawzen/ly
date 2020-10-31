82b0aea0f86fee283f49322140ea0a1eba4117a0
TpaeFawzen <30316430+GH-TpaeFawzen@users.noreply.github.com>
2020-10-31 20:28:35 +0900

    How about this optimization

--- res/wsetup.sh
+++ res/wsetup.sh
@@ -6,9 +6,8 @@
 # Copyright (C) 2001-2005 Oswald Buddenhagen <ossi@kde.org>
 
 # Note that the respective logout scripts are not sourced.
-case $SHELL in
-  */bash)
-    [ -z "$BASH" ] && exec $SHELL $0 "$@"
+case $SHELL in (*/bash)
+    case "$BASH" in ('') exec $SHELL $0 "$@"; esac
     set +o posix
     [ -f /etc/profile ] && . /etc/profile
     if [ -f $HOME/.bash_profile ]; then
@@ -18,9 +17,8 @@ case $SHELL in
     elif [ -f $HOME/.profile ]; then
       . $HOME/.profile
     fi
-    ;;
-*/zsh)
-    [ -z "$ZSH_NAME" ] && exec $SHELL $0 "$@"
+;;(*/zsh)
+    case "$ZSH_NAME" in ('') exec $SHELL $0 "$@"; esac
     [ -d /etc/zsh ] && zdir=/etc/zsh || zdir=/etc
     zhome=${ZDOTDIR:-$HOME}
     # zshenv is always sourced automatically.
@@ -29,26 +27,22 @@ case $SHELL in
     [ -f $zdir/zlogin ] && . $zdir/zlogin
     [ -f $zhome/.zlogin ] && . $zhome/.zlogin
     emulate -R sh
-    ;;
-  */csh|*/tcsh)
+;;(*/csh|*/tcsh)
     # [t]cshrc is always sourced automatically.
     # Note that sourcing csh.login after .cshrc is non-standard.
     wlsess_tmp=`mktemp /tmp/wlsess-env-XXXXXX`
     $SHELL -c "if (-f /etc/csh.login) source /etc/csh.login; if (-f ~/.login) source ~/.login; /bin/sh -c 'export -p' >! $wlsess_tmp"
     . $wlsess_tmp
     rm -f $wlsess_tmp
-    ;;
-  */fish)
+;;(*/fish)
     [ -f /etc/profile ] && . /etc/profile
     xsess_tmp=`mktemp /tmp/xsess-env-XXXXXX`
     $SHELL --login -c "/bin/sh -c 'export -p' > $xsess_tmp"
     . $xsess_tmp
     rm -f $xsess_tmp
-    ;;
-  *) # Plain sh, ksh, and anything we do not know.
+;;(*) # Plain sh, ksh, and anything we do not know.
     [ -f /etc/profile ] && . /etc/profile
     [ -f $HOME/.profile ] && . $HOME/.profile
-    ;;
-esac
+;;esac
 
 exec $@
